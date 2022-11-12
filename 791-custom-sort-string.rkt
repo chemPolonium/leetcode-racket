0a7209f5542cb9488e23cdd214099db4a65017c9
chemPolonium <chempolonium@outlook.com>
2022-11-13 01:15:29 +0800

    更新791六亲不认版

--- 791-custom-sort-string.rkt
+++ 791-custom-sort-string.rkt
@@ -15,14 +15,20 @@
   (< (hash-ref order-hash x 0)
      (hash-ref order-hash y 0)))
 
+; (define/contract (custom-sort-string order s)
+;   (-> string? string? string?)
+;   (let ([s-list (string->list s)]
+;         [order-hash (make-sort-hash order)])
+;     (let ([s-sorted-list (sort s-list
+;                                (lambda (x y)
+;                                  (less-than-order order-hash x y)))])
+;       (list->string s-sorted-list))))
+
 (define/contract (custom-sort-string order s)
   (-> string? string? string?)
-  (let ([s-list (string->list s)]
-        [order-hash (make-sort-hash order)])
-    (let ([s-sorted-list (sort s-list
-                               (lambda (x y)
-                                 (less-than-order order-hash x y)))])
-      (list->string s-sorted-list))))
+  (let* ([order-hash (make-hash (map cons (string->list order) (range (string-length order))))]
+         [less (lambda (x y) (< (hash-ref order-hash x 0) (hash-ref order-hash y 0)))])
+      (list->string (sort (string->list s) less))))
 
 (custom-sort-string "hwokrzpb" "hbwqpozrkp")
 
