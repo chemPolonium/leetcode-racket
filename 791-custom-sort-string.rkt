e7307da6f61a5e436b670a221f9778c23ca2eae9
chemPolonium <chempolonium@outlook.com>
2022-11-13 00:55:40 +0800

    791

---
+++ 791-custom-sort-string.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(define (make-sort-hash order)
+  (define (iter h s i)
+    (if (null? s)
+        h
+        (iter (hash-set h (car s) i)
+              (cdr s)
+              (add1 i))))
+  (iter (hash)
+        (string->list order)
+        0))
+
+(define (less-than-order order-hash x y)
+  (< (hash-ref order-hash x 0)
+     (hash-ref order-hash y 0)))
+
+(define/contract (custom-sort-string order s)
+  (-> string? string? string?)
+  (let ([s-list (string->list s)]
+        [order-hash (make-sort-hash order)])
+    (let ([s-sorted-list (sort s-list
+                               (lambda (x y)
+                                 (less-than-order order-hash x y)))])
+      (list->string s-sorted-list))))
+
+(custom-sort-string "hwokrzpb" "hbwqpozrkp")
+
+(custom-sort-string "exv" "xwvee")
