9e8405b6fc27dca3d8308d275c7439d4a036a831
chemPolonium <chempolonium@outlook.com>
2022-11-15 21:26:33 +0800

    41

---
+++ 41-first-missing-positive.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+(define/contract (first-missing-positive nums)
+  (-> (listof exact-integer?) exact-integer?)
+  (foldl (lambda (num res)
+           (if (= res num) (+ res 1) res)) 1 (sort nums)))
