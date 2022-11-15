1e16ac19c4f87745442c0d58578bcb6ff041da9a
chemPolonium <chempolonium@outlook.com>
2022-11-15 22:27:47 +0800

    mod 41 (not match the problem)

--- 41-first-missing-positive.rkt
+++ 41-first-missing-positive.rkt
@@ -3,4 +3,4 @@
 (define/contract (first-missing-positive nums)
   (-> (listof exact-integer?) exact-integer?)
   (foldl (lambda (num res)
-           (if (= res num) (+ res 1) res)) 1 (sort nums)))
+           (if (= res num) (+ res 1) res)) 1 (sort nums <)))
