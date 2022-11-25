a52e83ef19f0f53764af5615563bfb728ad6b2ab
chemPolonium <chempolonium@outlook.com>
2022-11-25 13:27:47 +0800

    mod 809

--- 809-expressive-words.rkt
+++ 809-expressive-words.rkt
@@ -1,12 +1,26 @@
 #lang racket
 
+; (define (string->pairs s)
+;   (define (iter i c sl res)
+;     (cond [(null? sl) (cons (cons c i) res)]
+;           [(or (false? c) (equal? c (car sl))) (iter (add1 i) (car sl) (cdr sl) res)]
+;           [else (iter 0 #f sl (cons (cons c i) res))]))
+;   (let ([slist (string->list s)])
+;     (iter 0 #f slist empty)))
+
+(define (extract-pair i c s)
+  (if (or (null? s) (not (equal? c (car s))))
+      (values i s)
+      (extract-pair (add1 i) c (cdr s))))
+
 (define (string->pairs s)
-  (define (iter i c sl res)
-    (cond [(null? sl) (cons (cons c i) res)]
-          [(or (false? c) (equal? c (car sl))) (iter (add1 i) (car sl) (cdr sl) res)]
-          [else (iter 0 #f sl (cons (cons c i) res))]))
-  (let ([slist (string->list s)])
-    (iter 0 #f slist empty)))
+  (define (iter s res)
+    (if (null? s)
+        res
+        (let ([c (car s)])
+          (let-values ([(i ss) (extract-pair 1 c (cdr s))])
+            (iter ss (cons (cons c i) res))))))
+  (iter (string->list s) empty))
 
 (define (expressive? spairs wpairs)
   (let ([sn (null? spairs)]
@@ -24,6 +38,8 @@
   (let* ([spairs (string->pairs s)]
          [sexpressive? (lambda (wpairs) (expressive? spairs wpairs))])
     (count sexpressive? (map string->pairs words))))
+
+(string->pairs "dddiiiinnssssssoooo")
 
 (expressive? (string->pairs "dddiiiinnssssssoooo")
              (string->pairs "ddiinnso"))
