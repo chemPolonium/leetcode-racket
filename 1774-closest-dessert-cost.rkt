efaf1f8a7b8210013e689c8ea3df0641a7fb3f2a
chemPolonium <chempolonium@outlook.com>
2022-12-04 01:39:23 +0800

    1774

---
+++ 1774-closest-dessert-cost.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(define (better a b)
+  (if (or (and (negative? a) (< (abs a) (abs b)))
+          (and (>= a 0) (<= a (abs b))))
+      a
+      b))
+
+(define (i-closest-res res toppingCosts)
+  (let ([s (for/fold ([s (set res)])
+                     ([t (in-list toppingCosts)])
+             (for/fold ([s1 s])
+                       ([r (in-set s)])
+               (cond [(positive? (- r t)) (set-add (set-add s1 (- r t)) (- r t t))]
+                     [(positive? r) (set-add s1 (- r t))]
+                     [else s1])))])
+    (for/fold ([m +inf.0])
+              ([r (in-set s)]
+               #:break (zero? m))
+      (better m r))))
+
+(define/contract (closest-cost baseCosts toppingCosts target)
+  (-> (listof exact-integer?) (listof exact-integer?) exact-integer? exact-integer?)
+  (define l
+    (map (lambda (baseCost) (i-closest-res (- target baseCost) toppingCosts)) baseCosts))
+  (- target (for/fold ([m +inf.0])
+                      ([r (in-list l)])
+              (better m r))))
+
+; (closest-cost '(1 7) '(3 4) 10)
+
+(closest-cost '(2 3) '(4 5 100) 18)
