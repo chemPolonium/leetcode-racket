01316ef55231b98b8c92fe2838e44dffd430e061
chemPolonium <chempolonium@outlook.com>
2023-02-11 22:32:30 +0800

    faster vector-search

--- vector-search.rkt
+++ vector-search.rkt
@@ -24,38 +24,63 @@
 (vector-binary-search #(1 3 5 7 9) 5)
 (vector-binary-search #(1 3 5 7 9) 10)
 
-; (define v (build-vector 10000000 identity))
-; (time (for/and ([i (in-range 10000000)])
-;         (vector-bs v i)))
+; (define (vector-lower-bound-s vec value [cmp -])
+;   (let iter ([current-ind 0]
+;              [hl-dis (vector-length vec)])
+;     (if (positive? hl-dis)
+;         (let* ([step (quotient hl-dis 2)]
+;                [next-ind (+ current-ind step)]
+;                [comparison (cmp (vector-ref vec next-ind) value)])
+;           (if (negative? comparison)
+;               (iter (add1 next-ind) (- hl-dis step 1))
+;               (iter current-ind step)))
+;         current-ind)))
 
 (define (vector-lower-bound vec value [cmp -])
-  (let iter ([current-ind 0]
-             [hl-dis (vector-length vec)])
-    (if (positive? hl-dis)
-        (let* ([step (quotient hl-dis 2)]
-               [next-ind (+ current-ind step)]
-               [comparison (cmp (vector-ref vec next-ind) value)])
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [comparison (cmp (vector-ref vec i) value)])
           (if (negative? comparison)
-              (iter (add1 next-ind) (- hl-dis step 1))
-              (iter current-ind step)))
-        current-ind)))
+              (iter (add1 i) end)
+              (iter start i))))))
 
 (vector-lower-bound #(1 3 5 7 9) 0)
 (vector-lower-bound #(1 3 5 7 9) 3)
 (vector-lower-bound #(1 3 5 7 9) 4)
 (vector-lower-bound #(1 3 5 7 9) 10)
 
+; (define v (build-vector 10000000 identity))
+
+; (time (for ([i (in-range 10000000)])
+;         (vector-lower-bound-s v i)))
+; (time (for ([i (in-range 10000000)])
+;         (vector-lower-bound v i)))
+
+; (define (vector-upper-bound vec value [cmp -])
+;   (let iter ([current-ind 0]
+;              [hl-dis (vector-length vec)])
+;     (if (positive? hl-dis)
+;         (let* ([step (quotient hl-dis 2)]
+;                [next-ind (+ current-ind step)]
+;                [comparison (cmp (vector-ref vec next-ind) value)])
+;           (if (positive? comparison)
+;               (iter current-ind step)
+;               (iter (add1 next-ind) (- hl-dis step 1))))
+;         current-ind)))
+
 (define (vector-upper-bound vec value [cmp -])
-  (let iter ([current-ind 0]
-             [hl-dis (vector-length vec)])
-    (if (positive? hl-dis)
-        (let* ([step (quotient hl-dis 2)]
-               [next-ind (+ current-ind step)]
-               [comparison (cmp (vector-ref vec next-ind) value)])
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [comparison (cmp (vector-ref vec i) value)])
           (if (positive? comparison)
-              (iter current-ind step)
-              (iter (add1 next-ind) (- hl-dis step 1))))
-        current-ind)))
+              (iter start i)
+              (iter (add1 i) end))))))
 
 (vector-upper-bound #(1 3 5 7 9) 0)
 (vector-upper-bound #(1 3 5 7 9) 3)
