d5c0fd088045e66b611688b6084153f03b9b87e6
chemPolonium <chempolonium@outlook.com>
2023-02-11 22:19:53 +0800

    add vector-upper-bound

--- vector-search.rkt
+++ vector-search.rkt
@@ -1,53 +1,63 @@
 #lang racket
 
-(define (vector-bs vec v [lo 0] [hi (vector-length vec)])
-  (and (<= (vector-ref vec lo) v)
-       (<= v (vector-ref vec (sub1 hi)))
-       (let iter ([lo lo] [hi hi])
-         (and (not (= lo hi))
-              (let* ([mi (quotient (+ lo hi) 2)]
-                     [m (vector-ref vec mi)])
-                (cond [(< m v) (iter (add1 mi) hi)]
-                      [(= m v) mi]
-                      [else (iter lo (sub1 mi))]))))))
+;;; (VECTOR-BINARY-SEARCH <vector> <value> <cmp>)
+;;;       -> exact, nonnegative integer or #F
+;;;     (CMP <value1> <value2>) -> integer
+;;;       positive -> VALUE1 > VALUE2
+;;;       zero     -> VALUE1 = VALUE2
+;;;       negative -> VALUE1 < VALUE2
+;;;   Perform a binary search through VECTOR for VALUE, comparing each
+;;;   element to VALUE with CMP.
+(define (vector-binary-search vec value [cmp -])
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        #f
+        (let* ([i (quotient (+ start end) 2)]
+               [comparison (cmp (vector-ref vec i) value)])
+          (cond [(positive? comparison) (iter start i)]
+                [(negative? comparison) (iter (add1 i) end)]
+                [else i])))))
 
-; (vector-bs #(1 3 5 7 9) 0)
-; (vector-bs #(1 3 5 7 9) 3)
-; (vector-bs #(1 3 5 7 9) 5)
-; (vector-bs #(1 3 5 7 9) 10)
-
-; (define (vector-lb vec v [lo 0] [hi (vector-length vec)])
-;   (cond [(<= v (vector-ref vec lo)) lo]
-;         [(< (vector-ref vec (sub1 hi)) v) #f]
-;         [else
-;          (let iter ([lo lo] [hi hi])
-;            (if (= (add1 lo) hi)
-;                hi
-;                (let* ([mi (quotient (+ lo hi 1) 2)]
-;                       [m (vector-ref vec mi)])
-;                  (if (< m v)
-;                      (iter mi hi)
-;                      (iter lo mi)))))]))
-
-(define (vector-lb vec v [lo 0] [hi (vector-length vec)])
-  (cond [(<= v (vector-ref vec lo)) lo]
-        [(< (vector-ref vec (sub1 hi)) v) #f]
-        [else
-         (let iter ([current-ind lo] [hl-dis (- hi lo)])
-           (if (positive? hl-dis)
-               (let* ([step (quotient hl-dis 2)]
-                      [next-ind (+ current-ind step)]
-                      [i (vector-ref vec next-ind)])
-                 (if (< i v)
-                     (iter (add1 next-ind) (- hl-dis step 1))
-                     (iter current-ind step)))
-               current-ind))]))
+(vector-binary-search #(1 3 5 7 9) 0)
+(vector-binary-search #(1 3 5 7 9) 3)
+(vector-binary-search #(1 3 5 7 9) 5)
+(vector-binary-search #(1 3 5 7 9) 10)
 
 ; (define v (build-vector 10000000 identity))
-; (time (for ([i (in-range 10000000)])
-;         (vector-lb v i)))
+; (time (for/and ([i (in-range 10000000)])
+;         (vector-bs v i)))
 
-; (vector-lb #(1 3 5 7 9) 0)
-; (vector-lb #(1 3 5 7 9) 3)
-; (vector-lb #(1 3 5 7 9) 4)
-; (vector-lb #(1 3 5 7 9) 10)
+(define (vector-lower-bound vec value [cmp -])
+  (let iter ([current-ind 0]
+             [hl-dis (vector-length vec)])
+    (if (positive? hl-dis)
+        (let* ([step (quotient hl-dis 2)]
+               [next-ind (+ current-ind step)]
+               [comparison (cmp (vector-ref vec next-ind) value)])
+          (if (negative? comparison)
+              (iter (add1 next-ind) (- hl-dis step 1))
+              (iter current-ind step)))
+        current-ind)))
+
+(vector-lower-bound #(1 3 5 7 9) 0)
+(vector-lower-bound #(1 3 5 7 9) 3)
+(vector-lower-bound #(1 3 5 7 9) 4)
+(vector-lower-bound #(1 3 5 7 9) 10)
+
+(define (vector-upper-bound vec value [cmp -])
+  (let iter ([current-ind 0]
+             [hl-dis (vector-length vec)])
+    (if (positive? hl-dis)
+        (let* ([step (quotient hl-dis 2)]
+               [next-ind (+ current-ind step)]
+               [comparison (cmp (vector-ref vec next-ind) value)])
+          (if (positive? comparison)
+              (iter current-ind step)
+              (iter (add1 next-ind) (- hl-dis step 1))))
+        current-ind)))
+
+(vector-upper-bound #(1 3 5 7 9) 0)
+(vector-upper-bound #(1 3 5 7 9) 3)
+(vector-upper-bound #(1 3 5 7 9) 4)
+(vector-upper-bound #(1 3 5 7 9) 10)
