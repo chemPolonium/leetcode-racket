0d03f4bd0cbcdf090ddf427b63ad424592a865ab
chemPolonium <chempolonium@outlook.com>
2023-02-12 16:42:53 +0800

    mod vector search

--- vector-search.rkt
+++ vector-search.rkt
@@ -1,14 +1,28 @@
 #lang racket
 
-;;; (VECTOR-BINARY-SEARCH <vector> <value> <cmp>)
-;;;       -> exact, nonnegative integer or #F
-;;;     (CMP <value1> <value2>) -> integer
-;;;       positive -> VALUE1 > VALUE2
-;;;       zero     -> VALUE1 = VALUE2
-;;;       negative -> VALUE1 < VALUE2
-;;;   Perform a binary search through VECTOR for VALUE, comparing each
-;;;   element to VALUE with CMP.
-(define (vector-binary-search vec value [cmp -])
+(define (vector-binary-search vec value)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        #f
+        (let* ([i (quotient (+ start end) 2)]
+               [it (vector-ref vec i)])
+          (cond [(> it value) (iter start i)]
+                [(< it value) (iter (add1 i) end)]
+                [else i])))))
+
+(define (vector-binary-search-key vec value key)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        #f
+        (let* ([i (quotient (+ start end) 2)]
+               [it (key (vector-ref vec i))])
+          (cond [(> it value) (iter start i)]
+                [(< it value) (iter (add1 i) end)]
+                [else i])))))
+
+(define (vector-binary-search-cmp vec value cmp)
   (let iter ([start 0]
              [end (vector-length vec)])
     (if (= start end)
@@ -19,24 +33,29 @@
                 [(negative? comparison) (iter (add1 i) end)]
                 [else i])))))
 
-(vector-binary-search #(1 3 5 7 9) 0)
-(vector-binary-search #(1 3 5 7 9) 3)
-(vector-binary-search #(1 3 5 7 9) 5)
-(vector-binary-search #(1 3 5 7 9) 10)
+(define (vector-lower-bound vec value)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [it (vector-ref vec i)])
+          (if (< it value)
+              (iter (add1 i) end)
+              (iter start i))))))
 
-; (define (vector-lower-bound-s vec value [cmp -])
-;   (let iter ([current-ind 0]
-;              [hl-dis (vector-length vec)])
-;     (if (positive? hl-dis)
-;         (let* ([step (quotient hl-dis 2)]
-;                [next-ind (+ current-ind step)]
-;                [comparison (cmp (vector-ref vec next-ind) value)])
-;           (if (negative? comparison)
-;               (iter (add1 next-ind) (- hl-dis step 1))
-;               (iter current-ind step)))
-;         current-ind)))
+(define (vector-lower-bound-key vec value key)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [it (key (vector-ref vec i))])
+          (if (< it value)
+              (iter (add1 i) end)
+              (iter start i))))))
 
-(define (vector-lower-bound vec value [cmp -])
+(define (vector-lower-bound-cmp vec value cmp)
   (let iter ([start 0]
              [end (vector-length vec)])
     (if (= start end)
@@ -47,17 +66,47 @@
               (iter (add1 i) end)
               (iter start i))))))
 
-(vector-lower-bound #(1 3 5 7 9) 0)
-(vector-lower-bound #(1 3 5 7 9) 3)
-(vector-lower-bound #(1 3 5 7 9) 4)
-(vector-lower-bound #(1 3 5 7 9) 10)
+(define (vector-upper-bound vec value)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [it (vector-ref vec i)])
+          (if (> it value)
+              (iter start i)
+              (iter (add1 i) end))))))
 
-; (define v (build-vector 10000000 identity))
+(define (vector-upper-bound-key vec value key)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [it (key (vector-ref vec i))])
+          (if (> it value)
+              (iter start i)
+              (iter (add1 i) end))))))
 
-; (time (for ([i (in-range 10000000)])
-;         (vector-lower-bound-s v i)))
-; (time (for ([i (in-range 10000000)])
-;         (vector-lower-bound v i)))
+(define (vector-upper-bound-cmp vec value cmp)
+  (let iter ([start 0]
+             [end (vector-length vec)])
+    (if (= start end)
+        start
+        (let* ([i (quotient (+ start end) 2)]
+               [comparison (cmp (vector-ref vec i) value)])
+          (if (positive? comparison)
+              (iter start i)
+              (iter (add1 i) end))))))
+
+;;; (VECTOR-BINARY-SEARCH <vector> <value> <cmp>)
+;;;       -> exact, nonnegative integer or #F
+;;;     (CMP <value1> <value2>) -> integer
+;;;       positive -> VALUE1 > VALUE2
+;;;       zero     -> VALUE1 = VALUE2
+;;;       negative -> VALUE1 < VALUE2
+;;;   Perform a binary search through VECTOR for VALUE, comparing each
+;;;   element to VALUE with CMP.
 
 ; (define (vector-upper-bound vec value [cmp -])
 ;   (let iter ([current-ind 0]
@@ -71,18 +120,43 @@
 ;               (iter (add1 next-ind) (- hl-dis step 1))))
 ;         current-ind)))
 
-(define (vector-upper-bound vec value [cmp -])
-  (let iter ([start 0]
-             [end (vector-length vec)])
-    (if (= start end)
-        start
-        (let* ([i (quotient (+ start end) 2)]
-               [comparison (cmp (vector-ref vec i) value)])
-          (if (positive? comparison)
-              (iter start i)
-              (iter (add1 i) end))))))
+; (define (vector-lower-bound-s vec value [cmp -])
+;   (let iter ([current-ind 0]
+;              [hl-dis (vector-length vec)])
+;     (if (positive? hl-dis)
+;         (let* ([step (quotient hl-dis 2)]
+;                [next-ind (+ current-ind step)]
+;                [comparison (cmp (vector-ref vec next-ind) value)])
+;           (if (negative? comparison)
+;               (iter (add1 next-ind) (- hl-dis step 1))
+;               (iter current-ind step)))
+;         current-ind)))
 
-(vector-upper-bound #(1 3 5 7 9) 0)
-(vector-upper-bound #(1 3 5 7 9) 3)
-(vector-upper-bound #(1 3 5 7 9) 4)
-(vector-upper-bound #(1 3 5 7 9) 10)
+(define (test-vector-search f)
+  (define v #(1 3 5 7 9))
+  (for ([i (in-list '(0 3 4 10))])
+    (displayln (f v i))))
+
+(define (time-vector-search f)
+  (define l 10000000)
+  (define v (build-vector l identity))
+  (time (for ([i (in-range l)])
+          (f v i))))
+
+(define (test-vector-binary-search)
+  (test-vector-search vector-binary-search))
+
+(define (test-vector-upper-bound)
+  (test-vector-search vector-upper-bound))
+
+(define (test-vector-lower-bound)
+  (test-vector-search vector-lower-bound))
+
+(define (time-vector-binary-search)
+  (time-vector-search vector-binary-search))
+
+(define (time-vector-binary-search-key)
+  (time-vector-search (λ (v i) (vector-binary-search-key v i identity))))
+
+(define (time-vector-binary-search-cmp)
+  (time-vector-search (λ (v i) (vector-binary-search-cmp v i -))))
