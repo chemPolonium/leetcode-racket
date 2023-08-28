d5cb0fcd029e1ad9fbf95b25111aeda7d97ad66b
chemPolonium <chempolonium@outlook.com>
2023-08-29 00:54:02 +0800

    optimize 823

--- 823-binary-trees-with-factors.rkt
+++ 823-binary-trees-with-factors.rkt
@@ -5,10 +5,14 @@
   (define sorted-arr (sort arr <))
   (define h (make-hasheq))
   (for ([i (in-list sorted-arr)])
-    (hash-set! h i
-               (add1 (for/sum ([j (in-list sorted-arr)] #:break (> j (sqrt i)))
-                       (* (if (= j (sqrt i)) 1 2)
-                          (* (hash-ref h j) (hash-ref h (/ i j) 0)))))))
+    (hash-set!
+     h i
+     (add1
+      (for/sum ([j (in-list sorted-arr)]
+                #:when (zero? (remainder i j))
+                #:break (> (* j j) i))
+        (* (if (= (* j j) i) 1 2)
+           (* (hash-ref h (quotient i j) 0) (hash-ref h j)))))))
   (remainder (apply + (hash-values h)) 1000000007))
 
 (num-factored-binary-trees '(2 4 5 10))
