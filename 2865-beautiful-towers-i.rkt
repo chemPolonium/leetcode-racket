fe55b9201392fa8f0a0a220994f9716b47664083
chemPolonium <chempolonium@outlook.com>
2024-01-24 19:40:28 +0800

    2865 mod

--- 2865-beautiful-towers-i.rkt
+++ 2865-beautiful-towers-i.rkt
@@ -7,23 +7,14 @@
     (define v (make-vector n))
     (for/fold ([stack null])
               ([(h i) (in-indexed (in-list hs))])
-      (cond [(zero? i)
-             (vector-set! v 0 h)
-             (list (cons h i))]
-            [(< (caar stack) h)
-             (vector-set! v i (+ (vector-ref v (sub1 i)) h))
-             (cons (cons h i) stack)]
-            [else
-             (define tail-stack
-               (memf (lambda (x)
-                       (< (car x) h))
-                     stack))
-             (if tail-stack
-                 (let ([step-ind (cdar tail-stack)])
-                   (vector-set! v i (+ (vector-ref v step-ind)
-                                       (* (- i step-ind) h))))
-                 (vector-set! v i (* (add1 i) h)))
-             (cons (cons h i) (or tail-stack null))]))
+      (define res-stack
+        (memf (lambda (x) (< (car x) h)) stack))
+      (if res-stack
+          (let ([step-ind (cdar res-stack)])
+            (vector-set! v i (+ (vector-ref v step-ind)
+                                (* (- i step-ind) h))))
+          (vector-set! v i (* (add1 i) h)))
+      (cons (cons h i) (or res-stack null)))
     (vector->list v))
   (foldl (lambda (a b c m) (max m (- (+ a b) c)))
          0
