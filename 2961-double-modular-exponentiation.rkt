c9eafd951357fcb7b0f8e2e4ae121f77d3299e37
chemPolonium <chempolonium@outlook.com>
2024-07-30 01:39:49 +0800

    2961 mod

--- 2961-double-modular-exponentiation.rkt
+++ 2961-double-modular-exponentiation.rkt
@@ -6,7 +6,6 @@
   (-> (listof (listof exact-integer?)) exact-integer? (listof exact-integer?))
   (define (target? a b c m)
     (= target (remainder (expt (remainder (expt a b) 10) c) m)))
-  (for/list ([i (in-naturals)]
-             [v (in-list variables)]
+  (for/list ([(v i) (in-indexed (in-list variables))]
              #:when (apply target? v))
     i))
