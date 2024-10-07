38f4086e1f061d7b8fdde91a8b914af5b1ffc365
chemPolonium <chempolonium@outlook.com>
2024-10-08 00:24:05 +0800

    1436

---
+++ 1436-destination-city.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(define/contract (dest-city paths)
+  (-> (listof (listof string?)) string?)
+  (define h (make-hash paths))
+  (let iter ([c (caar paths)])
+    (if (hash-has-key? h c) (iter (car (hash-ref h c))) c)))
