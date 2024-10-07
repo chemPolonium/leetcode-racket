9f1eaac014b8924fee8714e13a353a0fc7e9c330
chemPolonium <chempolonium@outlook.com>
2024-10-08 00:33:54 +0800

    1436 shorter

--- 1436-destination-city.rkt
+++ 1436-destination-city.rkt
@@ -2,6 +2,10 @@
 
 (define/contract (dest-city paths)
   (-> (listof (listof string?)) string?)
-  (define h (make-hash paths))
-  (let iter ([c (caar paths)])
-    (if (hash-has-key? h c) (iter (car (hash-ref h c))) c)))
+  (set-first (set-subtract (list->set (map cadr paths)) (list->set (map car paths)))))
+
+; (define/contract (dest-city paths)
+;   (-> (listof (listof string?)) string?)
+;   (define h (make-hash paths))
+;   (let iter ([c (caar paths)])
+;     (if (hash-has-key? h c) (iter (car (hash-ref h c))) c)))
