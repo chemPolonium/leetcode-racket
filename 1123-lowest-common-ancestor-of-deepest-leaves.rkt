17e86c72d191bb3c8c1a50dc0ced012f0f9c4a71
chemPolonium <chempolonium@outlook.com>
2025-04-04 13:39:30 +0800

    1123

--- 1123-lowest-common-ancestor-of-deepest-leaves.rkt
+++ 1123-lowest-common-ancestor-of-deepest-leaves.rkt
@@ -12,6 +12,19 @@
 
 (define/contract (lca-deepest-leaves root)
   (-> (or/c tree-node? #f) (or/c tree-node? #f))
+  (define (f2 root)
+    (if root
+        (let-values ([(l ld) (f2 (tree-node-left root))]
+                     [(r rd) (f2 (tree-node-right root))])
+          (cond [(> ld rd) (values l (add1 ld))]
+                [(< ld rd) (values r (add1 rd))]
+                [else (values root (add1 ld))]))
+        (values root 0)))
+  (define-values (r _) (f2 root))
+  r)
+
+(define/contract (lca-deepest-leaves-1 root)
+  (-> (or/c tree-node? #f) (or/c tree-node? #f))
   (define (f1 root)
     (define left (f (tree-node-left root)))
     (define right (f (tree-node-right root)))
